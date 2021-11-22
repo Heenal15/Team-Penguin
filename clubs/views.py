4e59bdb84ac065bb60189998feb77ed083239e02
Abdurrahman LLeshi <k20014224@kcl.ac.uk>
2021-11-22 20:43:00 +0000

    Add homepage, login page, registration page

--- clubs/views.py
+++ clubs/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def log_in(request):
+    return render(request, 'log_in.html')
+
+def home(request):
+    return render(request, 'home.html')
+
+def register(request):
+    return render(request, 'register.html')
