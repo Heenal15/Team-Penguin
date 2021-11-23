e55fd9f6b233bb155195f88d215a520f5df3fd48
“Heenal <heenal.vyas@kcl.ac.uk>
2021-11-23 13:28:15 +0000

    Create user model and test cases

--- clubs/models.py
+++ clubs/models.py
@@ -1,3 +1,36 @@
-from django.db import models
+"""Models in the clubs app."""
 
-# Create your models here.
+from django.core.validators import RegexValidator
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from libgravatar import Gravatar
+
+class User(AbstractUser):
+    """User model used for authentication and club authoring."""
+    username = models.CharField(
+        max_length=30,
+        unique=True,
+        validators=[RegexValidator(
+            regex=r'^@\w{3,}$',
+            message='Username must consist of @ followed by at least three alphanumericals'
+        )]
+    )
+    first_name = models.CharField(max_length=50, blank=False)
+    last_name = models.CharField(max_length=50, blank=False)
+    email = models.EmailField(unique=True, blank=False)
+    bio = models.CharField(max_length=520, blank=True)
+    experience = models.CharField(max_length=300, blank=True)
+    statement = models.CharField(max_length=1000, blank=True)
+
+    def full_name(self):
+        return f'{self.first_name} {self.last_name}'
+
+    def gravatar(self, size=120):
+        """Return a URL to the user's gravatar."""
+        gravatar_object = Gravatar(self.email)
+        gravatar_url = gravatar_object.get_image(size=size, default='mp')
+        return gravatar_url
+
+    def mini_gravatar(self):
+        """Return a URL to a miniature version of the user's gravatar."""
+        return self.gravatar(size=60)
--- system/settings.py
+++ system/settings.py
@@ -124,3 +124,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# User model for authentication and login purposes
+AUTH_USER_MODEL = 'clubs.User'
