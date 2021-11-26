4616a2f3dbb55130807d184d8bacdb589219b833
abbenayanjey <78971336+abbenayanjey@users.noreply.github.com>
2021-11-26 00:44:29 +0000

    Added basic seeding of users to display user list

--- clubs/management/commands/seed.py
+++ clubs/management/commands/seed.py
@@ -1,7 +1,56 @@
 from django.core.management.base import BaseCommand, CommandError
+from faker import Faker
+from clubs.models import User
 
 class Command(BaseCommand):
     """The database seeder."""
+    PASSWORD = "Password123"
+    USER_COUNT = 100
+
+    def __init__(self):
+        super().__init__()
+        self.faker = Faker('en_GB')
+
 
     def handle(self, *args, **options):
-        print("TODO: The database seeder will be added here...")
+        user_count = 0
+        while user_count < Command.USER_COUNT:
+            print(f'Seeding user {user_count}',  end='\r')
+            try:
+                self._create_user()
+            except (django.db.utils.IntegrityError):
+                continue
+            user_count += 1
+        print('User seeding complete')
+
+    def _create_user(self):
+        first_name = self.faker.first_name()
+        last_name = self.faker.last_name()
+        email = self._email(first_name, last_name)
+        username = self._username(first_name, last_name)
+        bio = self.faker.text(max_nb_chars=520)
+        statement = self.faker.text(max_nb_chars=520)
+        experience = self._experience()
+        User.objects.create_user(
+            username,
+            first_name=first_name,
+            last_name=last_name,
+            email=email,
+            password=Command.PASSWORD,
+            bio=bio,
+            statement=statement,
+            experience=experience,
+        )
+
+    def _email(self, first_name, last_name):
+        email = f'{first_name}.{last_name}@example.org'
+        return email
+
+    def _username(self, first_name, last_name):
+        username = f'@{first_name}{last_name}'
+        return username
+        
+    # Method to set experience level.
+    def _experience(self):
+        level = 'Member'
+        return level
--- clubs/management/commands/unseed.py
+++ clubs/management/commands/unseed.py
@@ -1,7 +1,7 @@
 from django.core.management.base import BaseCommand, CommandError
+from clubs.models import User
 
 class Command(BaseCommand):
         """The database unseeder."""
-
         def handle(self, *args, **options):
-            print("TODO: The database unseeder will be added here...")
+            User.objects.filter(is_staff=False, is_superuser=False).delete()
