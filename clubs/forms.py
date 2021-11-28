00f5a5ed56220fa041a2d538c38c550400db4637
“Heenal <heenal.vyas@kcl.ac.uk>
2021-11-28 22:45:15 +0000

    Change user password and test cases

--- clubs/forms.py
+++ clubs/forms.py
@@ -30,17 +30,6 @@ class ApplicationForm(forms.ModelForm):
         )
     password_confirmation = forms.CharField(label='Password Confirmation', widget=forms.PasswordInput())
 
-class UserForm(forms.ModelForm):
-    """Form to update user profiles."""
-
-    class Meta:
-        """Form options."""
-
-        model = User
-        fields = ['first_name', 'last_name', 'email', 'bio', 'experience', 'statement']
-        widgets = { 'bio': forms.Textarea(), 'experience':forms.Textarea(), 'statement':forms.Textarea()}
-
-
     def clean(self):
         super().clean()
         new_password = self.cleaned_data.get('new_password')
@@ -60,3 +49,37 @@ class UserForm(forms.ModelForm):
             password=self.cleaned_data.get('new_password')
         )
         return user
+
+class UserForm(forms.ModelForm):
+    """Form to update user profiles."""
+
+    class Meta:
+        """Form options."""
+
+        model = User
+        fields = ['first_name', 'last_name', 'email', 'bio', 'experience', 'statement']
+        widgets = { 'bio': forms.Textarea(), 'experience':forms.Textarea(), 'statement':forms.Textarea()}
+
+class PasswordForm(forms.Form):
+    """Form enabling users to change their password."""
+
+    password = forms.CharField(label='Current password', widget=forms.PasswordInput())
+    new_password = forms.CharField(
+        label='Password',
+        widget=forms.PasswordInput(),
+        validators=[RegexValidator(
+            regex=r'^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9]).*$',
+            message='Password must contain an uppercase character, a lowercase '
+                    'character and a number'
+            )]
+    )
+    password_confirmation = forms.CharField(label='Password confirmation', widget=forms.PasswordInput())
+
+    def clean(self):
+        """Clean the data and generate messages for any errors."""
+
+        super().clean()
+        new_password = self.cleaned_data.get('new_password')
+        password_confirmation = self.cleaned_data.get('password_confirmation')
+        if new_password != password_confirmation:
+            self.add_error('password_confirmation', 'Confirmation does not match password.')
