e8c771497c81f807837d86e3fe0d3f15ce08d4d9
abbenayanjey <78971336+abbenayanjey@users.noreply.github.com>
2021-12-03 19:27:57 +0000

    Update seed.py for new user model

--- clubs/management/commands/seed.py
+++ clubs/management/commands/seed.py
@@ -28,10 +28,11 @@ class Command(BaseCommand):
         first_name = self.faker.first_name()
         last_name = self.faker.last_name()
         email = self._email(first_name, last_name)
+        user_type=random.randint(0, 2)
 
         bio = self.faker.text(max_nb_chars=520)
         statement = self.faker.text(max_nb_chars=520)
-        experience = self._experience()
+        experience = self.faker.text(max_nb_chars=20)
         User.objects.create_user(
 
             first_name=first_name,
@@ -41,23 +42,9 @@ class Command(BaseCommand):
             bio=bio,
             statement=statement,
             experience=experience,
-            user_type=random.randint(1, 4),
+            user_type=user_type,
         )
 
     def _email(self, first_name, last_name):
         email = f'{first_name}.{last_name}@example.org'
         return email
-
-    def _username(self, first_name, last_name):
-        username = f'@{first_name}{last_name}'
-        return username
-
-    # Method to set experience level.
-    def _experience(self):
-        member = 'Member'
-        officer = 'Officer'
-        random_number = random.randint(1, 4)
-        if random_number != 1:
-            return member
-        else:
-            return officer
