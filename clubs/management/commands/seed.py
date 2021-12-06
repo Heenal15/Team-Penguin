351cc798b397d5169c50d6c268df1ff7ce6e2dcb
abbenayanjey <78971336+abbenayanjey@users.noreply.github.com>
2021-12-06 19:18:29 +0000

    Update seed.py with preset users

--- clubs/management/commands/seed.py
+++ clubs/management/commands/seed.py
@@ -23,45 +23,100 @@ class Command(BaseCommand):
                 continue
             user_count += 1
         print('User seeding complete')
+        self._create_member()
+        print('Member: Jebediah Kerman has been created')
+        self._create_officer()
+        print('Officer: Valentina Kerman has been created')
+        self._create_owner()
+        print('Owner: Billie Kerman has been created')
 
     def _create_user(self):
         first_name = self.faker.first_name()
         last_name = self.faker.last_name()
         email = self._email(first_name, last_name)
         user_type=random.randint(0, 2)
-
         bio = self.faker.text(max_nb_chars=520)
         statement = self.faker.text(max_nb_chars=520)
-        experience = self.faker.text(max_nb_chars=20)
+        experience = self._experience()
         User.objects.create_user(
 
             first_name=first_name,
             last_name=last_name,
             email=email,
+            user_type=user_type,
             password=Command.PASSWORD,
             bio=bio,
             statement=statement,
             experience=experience,
-            user_type=user_type,
+
         )
 
     def _email(self, first_name, last_name):
         email = f'{first_name}.{last_name}@example.org'
         return email
-<<<<<<< HEAD
 
-    def _username(self, first_name, last_name):
-        username = f'@{first_name}{last_name}'
-        return username
-
-    # Method to set experience level.
     def _experience(self):
-        member = 'Member'
-        officer = 'Officer'
-        random_number = random.randint(1, 4)
-        if random_number != 2:
-            return member
-        else:
-            return officer
-=======
->>>>>>> main
+        experience_list = ['Beginner', 'Intermediate', 'Advanced']
+        expereince = random.choice(experience_list)
+        return expereince
+
+    def _create_member(self):
+        first_name = 'Jebediah'
+        last_name = 'Kerman'
+        email = 'jeb@example.org'
+        user_type = 1
+        bio = self.faker.text(max_nb_chars=520)
+        statement = self.faker.text(max_nb_chars=520)
+        experience = 'Beginner'
+        User.objects.create_user(
+
+            first_name=first_name,
+            last_name=last_name,
+            email=email,
+            user_type=user_type,
+            password=Command.PASSWORD,
+            bio=bio,
+            statement=statement,
+            experience=experience,
+
+        )
+    def _create_officer(self):
+        first_name = 'Valentina'
+        last_name = 'Kerman'
+        email = 'val@example.org'
+        user_type = 2
+        bio = self.faker.text(max_nb_chars=520)
+        statement = self.faker.text(max_nb_chars=520)
+        experience = 'Intermediate'
+        User.objects.create_user(
+
+            first_name=first_name,
+            last_name=last_name,
+            email=email,
+            user_type=user_type,
+            password=Command.PASSWORD,
+            bio=bio,
+            statement=statement,
+            experience=experience,
+
+        )
+    def _create_owner(self):
+        first_name = 'Billie'
+        last_name = 'Kerman'
+        email = 'billie@example.org'
+        user_type = 3
+        bio = self.faker.text(max_nb_chars=520)
+        statement = self.faker.text(max_nb_chars=520)
+        experience = 'Advanced'
+        User.objects.create_user(
+
+            first_name=first_name,
+            last_name=last_name,
+            email=email,
+            user_type=user_type,
+            password=Command.PASSWORD,
+            bio=bio,
+            statement=statement,
+            experience=experience,
+
+        )
