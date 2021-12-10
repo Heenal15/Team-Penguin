1028b4837b3abc4ca2d576765277adb6ce28bd60
Heenal15 <78482808+Heenal15@users.noreply.github.com>
2021-12-10 23:27:56 +0000

    Update urls.py

--- system/urls.py
+++ system/urls.py
@@ -42,4 +42,5 @@ urlpatterns = [
     path('club_list/', views.club_list, name='club_list'),
     path('create_club/', views.create_club, name='create_club'),
     path('load_club/', views.load_club, name='load_club'),
+]
 
