83934f6f37d7b62d208755cc81205ce7729fb169
“Heenal <heenal.vyas@kcl.ac.uk>
2021-12-11 17:26:00 +0000

    Add show_user view tests

---
+++ clubs/tests/test_show_user_view.py
@@ -0,0 +1,47 @@
+from django.test import TestCase
+from django.urls import reverse
+from clubs.models import User
+
+class ShowUserTest(TestCase):
+
+    fixtures = [
+        'clubs/tests/fixtures/default_user.json',
+        'clubs/tests/fixtures/other_users.json'
+    ]
+
+    def setUp(self):
+        self.user = User.objects.get(email='emilydoe@example.org')
+        self.target_user = User.objects.get(email='mikedoe@example.org')
+        self.url = reverse('show_user', kwargs={'user_id': self.target_user.id})
+
+    def test_show_user_url(self):
+        self.assertEqual(self.url,f'/user/{self.target_user.id}')
+
+    def test_get_show_user_with_valid_id(self):
+        self.client.login(email=self.user.email, password='Password123')
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'show_user.html')
+        self.assertContains(response, "Mike Doe")
+        self.assertContains(response, "mikedoe@example.org")
+
+    def test_get_show_user_with_own_id(self):
+        self.client.login(email=self.user.email, password='Password123')
+        url = reverse('show_user', kwargs={'user_id': self.user.id})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'show_user.html')
+        self.assertContains(response, "Emily Doe")
+        self.assertContains(response, "emilydoe@example.org")
+
+    def test_get_show_user_with_invalid_id(self):
+        self.client.login(email=self.user.email, password='Password123')
+        url = reverse('show_user', kwargs={'user_id': self.user.id+9999})
+        response = self.client.get(url, follow=True)
+        response_url = reverse('home')
+        self.assertRedirects(response, response_url, status_code=302, target_status_code=200)
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_get_show_user_redirects_when_not_logged_in(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 302)
