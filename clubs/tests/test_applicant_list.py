1a7da5b152bb70c1fe7e1fa07ac46f7e76dcee30
“Heenal <heenal.vyas@kcl.ac.uk>
2021-12-11 16:46:38 +0000

    Fix all tests

--- clubs/tests/test_applicant_list.py
+++ clubs/tests/test_applicant_list.py
@@ -3,17 +3,13 @@ from django.urls import reverse
 from clubs.models import User
 
 class ApplicantListTest(TestCase):
+
+    fixtures = ['clubs/tests/fixtures/other_users.json']
+
     def setUp(self):
         self.url = reverse('applicant_list')
-        self.user = User.objects.create_user(
-            first_name = 'John',
-            last_name = 'Doe',
-            email = 'johndoe@example.org',
-            password = 'Password123',
-            bio = 'Hello, I am John Doe',
-            is_active = True,
-            user_type = 2
-        )
+
+        self.user = User.objects.get(email='emilydoe@example.org')
         self.client = Client()
 
     def test_applicant_list_url(self):
@@ -24,21 +20,21 @@ class ApplicantListTest(TestCase):
         self.assertEqual(response.status_code, 302)
 
     def test_get_applicant_list_as_authorised_user(self):
-        self.client.login(email='johndoe@example.org', password='Password123')
-        self._create_test_applicants(5-1)
+        self.client.login(email=self.user.email, password='Password123')
+        self._create_test_applicants(15-1)
         response = self.client.get(self.url)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'applicant_list.html')
-        self.assertEqual(len(response.context['applicants']), 5)
-        for user_id in range(5-1):
-            self.assertContains(response, f'Email{user_id}')
+        self.assertEqual(len(response.context['applicants']), 15)
+        for user_id in range(15-1):
+            self.assertContains(response, f'Email{user_id}@test.org')
             self.assertContains(response, f'First{user_id}')
             self.assertContains(response, f'Last{user_id}')
-            user = User.objects.get(email=f'Email{user_id}')
+            user = User.objects.get(email=f'Email{user_id}@test.org')
             user_url = reverse('show_user', kwargs={'user_id': user.id})
             self.assertContains(response, user_url)
 
-    def _create_test_applicants(self, user_count=5):
+    def _create_test_applicants(self, user_count=4):
         for user_id in range(user_count):
             User.objects.create_user(f'Email{user_id}@test.org',
                 password='Password123',
@@ -46,5 +42,6 @@ class ApplicantListTest(TestCase):
                 last_name=f'Last{user_id}',
                 bio=f'Bio {user_id}',
                 experience=f'Experience {user_id}',
-                statement=f'Statement {user_id}'
+                statement=f'Statement {user_id}',
+                user_type = 0
             )
