3b9b676a2a4066e5cf59acc89792c04d976a6ae8
abbenayanjey <78971336+abbenayanjey@users.noreply.github.com>
2021-12-13 01:05:54 +0000

    Update unseed.py

--- clubs/management/commands/unseed.py
+++ clubs/management/commands/unseed.py
@@ -1,8 +1,9 @@
 from django.core.management.base import BaseCommand, CommandError
-from clubs.models import User, Club
+from clubs.models import User, Club, ClubContract
 
 class Command(BaseCommand):
         """The database unseeder."""
         def handle(self, *args, **options):
             User.objects.filter(is_staff=False, is_superuser=False).delete()
-            # Club.objects.filter(club_name!='Kerbal Chess Club').delete()
+            Club.objects.all().delete()
+            ClubContract.objects.all().delete()
