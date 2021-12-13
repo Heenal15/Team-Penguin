c9aeea4b3099c350e3011841997ddf101d1f7c4b
abbenayanjey <78971336+abbenayanjey@users.noreply.github.com>
2021-12-13 01:05:49 +0000

    Update seed.py for new models

--- clubs/management/commands/seed.py
+++ clubs/management/commands/seed.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand, CommandError
 from faker import Faker
-from clubs.models import User, Club
+from clubs.models import User, Club, ClubContract
 import random
 
 class Command(BaseCommand):
@@ -8,6 +8,7 @@ class Command(BaseCommand):
     PASSWORD = "Password123"
     USER_COUNT = 100
     CLUB_COUNT = 3
+    CONTRACT_COUNT = 200
 
     def __init__(self):
         super().__init__()
@@ -43,12 +44,25 @@ class Command(BaseCommand):
         self._create_owner()
         print('Owner: Billie Kerman has been created')
 
+        self.users = User.objects.all()
+        self.allClubs = Club.objects.all()
+        contract_count = 0
+        while contract_count < Command.CONTRACT_COUNT:
+            print(f'Seeding contract {contract_count}',  end='\r')
+            try:
+                self._create_contract()
+            except (django.db.utils.IntegrityError):
+                continue
+            contract_count += 1
+        self.assign_billie_to_kerbal()
+        print('Billie is the owner of Kerbal Chess Club')
+        print('Contract seeding is complete')
 
-    def _create_user(self):
+def _create_user(self):
         first_name = self.faker.first_name()
         last_name = self.faker.last_name()
         email = self._email(first_name, last_name)
-        user_type=random.randint(0, 2)
+        # user_type=random.randint(0, 2)
         bio = self.faker.text(max_nb_chars=520)
         statement = self.faker.text(max_nb_chars=520)
         experience = self._experience()
@@ -57,7 +71,7 @@ class Command(BaseCommand):
             first_name=first_name,
             last_name=last_name,
             email=email,
-            user_type=user_type,
+            # user_type=user_type,
             password=Command.PASSWORD,
             bio=bio,
             statement=statement,
@@ -78,7 +92,7 @@ class Command(BaseCommand):
         first_name = 'Jebediah'
         last_name = 'Kerman'
         email = 'jeb@example.org'
-        user_type = 1
+        # user_type = 1
         bio = self.faker.text(max_nb_chars=520)
         statement = self.faker.text(max_nb_chars=520)
         experience = 'Beginner'
@@ -87,7 +101,7 @@ class Command(BaseCommand):
             first_name=first_name,
             last_name=last_name,
             email=email,
-            user_type=user_type,
+            # user_type=user_type,
             password=Command.PASSWORD,
             bio=bio,
             statement=statement,
@@ -98,7 +112,7 @@ class Command(BaseCommand):
         first_name = 'Valentina'
         last_name = 'Kerman'
         email = 'val@example.org'
-        user_type = 2
+        # user_type = 2
         bio = self.faker.text(max_nb_chars=520)
         statement = self.faker.text(max_nb_chars=520)
         experience = 'Intermediate'
@@ -107,7 +121,7 @@ class Command(BaseCommand):
             first_name=first_name,
             last_name=last_name,
             email=email,
-            user_type=user_type,
+            # user_type=user_type,
             password=Command.PASSWORD,
             bio=bio,
             statement=statement,
@@ -118,7 +132,7 @@ class Command(BaseCommand):
         first_name = 'Billie'
         last_name = 'Kerman'
         email = 'billie@example.org'
-        user_type = 3
+        # user_type = 3
         bio = self.faker.text(max_nb_chars=520)
         statement = self.faker.text(max_nb_chars=520)
         experience = 'Advanced'
@@ -127,7 +141,7 @@ class Command(BaseCommand):
             first_name=first_name,
             last_name=last_name,
             email=email,
-            user_type=user_type,
+            # user_type=user_type,
             password=Command.PASSWORD,
             bio=bio,
             statement=statement,
@@ -158,3 +172,31 @@ class Command(BaseCommand):
             club_description = club_description,
 
         )
+
+    def _create_contract(self):
+        contract = ClubContract()
+        contract.user = self.get_random_user()
+        contract.club = self.get_random_club()
+        contract.role = self.get_random_role()
+        contract.save()
+
+    def get_random_user(self):
+        self.users = User.objects.all()
+        index = random.randint(0,self.users.count()-1)
+        return self.users[index]
+
+    def get_random_club(self):
+        self.allClubs = Club.objects.all()
+        index = random.randint(0,self.allClubs.count()-1)
+        return self.allClubs[index]
+
+    def get_random_role(self):
+        random_number = random.randint(0,3)
+        return random_number
+
+    def assign_billie_to_kerbal(self):
+        contract = ClubContract()
+        contract.user = User.objects.get(first_name = 'Billie')
+        contract.club = Club.objects.get(club_name='Kerbal Chess Club')
+        contract.role = 3
+        contract.save()
