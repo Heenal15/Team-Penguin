290a262375d0950107fe0d67bfb696657379a040
Kai Cor <kai.corr@kcl.ac.uk>
2021-12-16 12:05:33 +0000

    Move promotion/demotion/ownership-transfer into a seperate view file

--- clubs/views/__init__.py
+++ clubs/views/__init__.py
@@ -4,4 +4,5 @@ from .club_views import *
 from .homepage_views import *
 from .list_user_views import *
 from .static_views import *
+from .user_type_change_view import *
 from .user_type_check import *
