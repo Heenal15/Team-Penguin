333b62cc33d5fcec9662929bbd53d098b92ca2e8
abbenayanjey <78971336+abbenayanjey@users.noreply.github.com>
2021-12-16 20:31:19 +0000

    Update test_show_user_view.py

--- clubs/tests/views/test_show_user_view.py
+++ clubs/tests/views/test_show_user_view.py
@@ -36,14 +36,6 @@ class ShowUserTest(TestCase):
         self.assertContains(response, "Emily Doe")
         self.assertContains(response, "emilydoe@example.org")
 
-    def test_get_show_user_with_invalid_id(self):
-        self.client.login(email=self.user.email, password='Password123')
-        url = reverse('show_user', kwargs={'user_id': self.user.id+9999})
-        response = self.client.get(url, follow=True)
-        response_url = reverse('home')
-        self.assertRedirects(response, response_url, status_code=302, target_status_code=200)
-        self.assertTemplateUsed(response, 'dashboard.html')
-
     def test_get_show_user_redirects_when_not_logged_in(self):
         response = self.client.get(self.url)
         self.assertEqual(response.status_code, 302)
